fff9052ef2ddf21790268cd239847cabb1152de5
dao0203 <youzaizuoteng872@gmail.com>
2024-03-15 15:33:50 +0900

    add delegation pattern

---
+++ src/main/kotlin/adapter/delegationpattern/PrintBanner.kt
@@ -0,0 +1,17 @@
+package adapter.delegationpattern
+
+import adapter.Banner
+
+class PrintBanner(
+    private val string: String
+) : Print() {
+    private val banner = Banner(string)
+    override fun printWeak() {
+        banner.showWithParen()
+    }
+
+    override fun printStrong() {
+        banner.showWithAster()
+    }
+
+}
