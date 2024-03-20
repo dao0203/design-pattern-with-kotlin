50c344f0c6a146632c32971b681d00c44bda56bd
dao0203 <youzaizuoteng872@gmail.com>
2024-03-20 16:26:01 +0900

    learn builder

---
+++ src/main/kotlin/builder/TextBuilder.kt
@@ -0,0 +1,30 @@
+package builder
+
+class TextBuilder : Builder() {
+    private val buffer = StringBuffer()
+    override fun makeTitle(title: String) {
+        buffer.append("==============================\n")
+        buffer.append("『$title』\n")
+        buffer.append("\n")
+    }
+
+    override fun makeString(str: String) {
+        buffer.append("■$str\n")
+        buffer.append("\n")
+    }
+
+    override fun makeItems(items: List<String>) {
+        for (item in items) {
+            buffer.append(" ・$item\n")
+        }
+        buffer.append("\n")
+    }
+
+    override fun close() {
+        buffer.append("==============================\n")
+    }
+
+    fun getResult(): String {
+        return buffer.toString()
+    }
+}
